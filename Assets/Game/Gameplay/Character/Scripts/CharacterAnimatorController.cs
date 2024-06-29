017773826c439a03937e1078683486b9c73d92f9
GolubVR <74712885+GolubVR@users.noreply.github.com>
2024-06-29 17:57:50 +0700

    Add interact animation

--- Assets/Game/Gameplay/Character/Scripts/CharacterAnimatorController.cs
+++ Assets/Game/Gameplay/Character/Scripts/CharacterAnimatorController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using DoubleDTeam.Attributes;
 using UnityEngine;
+using UnityEngine.Events;
 
 [RequireComponent(typeof(Animator))]
 public class CharacterAnimatorController : MonoBehaviour
@@ -12,25 +13,30 @@ public class CharacterAnimatorController : MonoBehaviour
     [SerializeField] private float _animationSpeedMultiplier;
     
     private static readonly int Speed = Animator.StringToHash("Speed");
+    private static readonly int Interact = Animator.StringToHash("Interact");
 
-    private void OnValidate()
-    {
+    public UnityAction OnEndInteract;
+
+    private void OnValidate() => 
         _animator = GetComponent<Animator>();
-    }
 
-    private void OnEnable()
-    {
+    private void OnEnable() => 
         _mover.SpeedChanged += ChangeAnimationSpeed;
-    }
 
-    private void OnDisable()
-    {
+    private void OnDisable() => 
         _mover.SpeedChanged -= ChangeAnimationSpeed;
-    }
+
+    public void StartInteract() => 
+        _animator.SetTrigger(Interact);
+
+    public void OnAnimationStopInteract() =>
+        OnEndInteract?.Invoke();
+
 
     private void ChangeAnimationSpeed(float speed)
     {
         speed *= _animationSpeedMultiplier;
         _animator.SetFloat(Speed, speed);
     }
+
 }
