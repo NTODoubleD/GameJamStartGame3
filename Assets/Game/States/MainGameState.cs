f04f9fd3d8e0e70a9e6b33adc6e34c78b03b492b
Leonid Medvedev <leonid_med@mail.ru>
2024-06-29 20:34:56 +0300

    Add main menu

---
+++ Assets/Game/States/MainGameState.cs
@@ -0,0 +1,25 @@
+﻿using DoubleDTeam.InputSystem;
+using DoubleDTeam.StateMachine.Base;
+using Game.InputMaps;
+
+namespace Game.States
+{
+    public class MainGameState : IState
+    {
+        private readonly InputController _inputController;
+
+        public MainGameState(InputController inputController)
+        {
+            _inputController = inputController;
+        }
+
+        public void Enter()
+        {
+            _inputController.EnableMap<PlayerInputMap>();
+        }
+
+        public void Exit()
+        {
+        }
+    }
+}
---
+++ Assets/Game/States/MainMenuState.cs
@@ -0,0 +1,30 @@
+﻿using DoubleDTeam.InputSystem;
+using DoubleDTeam.StateMachine.Base;
+using DoubleDTeam.UI.Base;
+using Game.InputMaps;
+using Game.UI;
+
+namespace Game.States
+{
+    public class MainMenuState : IState
+    {
+        private readonly InputController _inputController;
+        private readonly IUIManager _uiManager;
+
+        public MainMenuState(InputController inputController, IUIManager uiManager)
+        {
+            _inputController = inputController;
+            _uiManager = uiManager;
+        }
+
+        public void Enter()
+        {
+            _inputController.EnableMap<UIInputMap>();
+            _uiManager.OpenPage<MainMenuPage>();
+        }
+
+        public void Exit()
+        {
+        }
+    }
+}
