c0430bf5e3820254abc9e4d05e2e6f5423d427fc
Lexv0lk <Lexvolk2004@yandex.ru>
2024-06-29 21:25:50 +0400

    Сделал основной инвентарь и работу условий с ресурсами

--- Assets/Game/Infrastructure/Storage/ItemStorage.cs
+++ Assets/Game/Infrastructure/Storage/ItemStorage.cs
@@ -1,198 +1,59 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using DoubleDTeam.Extensions;
+using DoubleDTeam.Containers.Base;
 using Game.Infrastructure.Items;
 using UnityEngine;
 
 namespace Game.Infrastructure.Storage
 {
-    public class ItemStorage
+    public class ItemStorage : IModule
     {
-        private bool _enable;
+        private readonly Dictionary<ItemInfo, int> _items = new();
 
-        public event Action<bool> EnableStateChanged;
-
-        public bool Enable
+        public ItemStorage()
         {
-            get => _enable;
-            set
-            {
-                _enable = value;
-                EnableStateChanged?.Invoke(_enable);
-            }
+
         }
-
-        private readonly List<Item> _items;
-
-        public readonly ItemStorageInfo Info;
-
-        public event Action<Item> ItemAdded;
-        public event Action<Item> ItemPushed;
-        public event Action<Item> ItemPopped;
-
-        public int AmountItems => _items.Count;
-
-        public IReadOnlyList<Item> Items => _items;
 
         public ItemStorage(ItemStorageInfo info)
         {
-            _enable = true;
-
-            Info = info;
-            _items = new List<Item>();
+            foreach (var keyPair in info.StartItems)
+                AddItems(keyPair.Key, keyPair.Value);
         }
 
-        public void AddItem(Item item)
+        public event Action<ItemInfo, int> ItemAdded;
+        public event Action<ItemInfo, int> ItemRemoved;
+
+        public void AddItems(ItemInfo item, int count)
         {
-            if (CanAddItem(item) == false)
-                return;
+            if (_items.ContainsKey(item))
+                _items[item] += count;
+            else
+                _items.Add(item, count);
 
-            _items.Add(item);
-
-            ItemAdded?.Invoke(item);
+            ItemAdded?.Invoke(item, count);
         }
 
-        public void AddItems(IEnumerable<Item> items)
+        public void RemoveItems(ItemInfo item, int count)
         {
-            if (Enable == false)
-                return;
-
-            var arrayItems = items as Item[] ?? items.ToArray();
-
-            if (CanAddItems(arrayItems) == false)
-                return;
-
-            foreach (var item in arrayItems)
-                AddItem(item);
+            _items[item] = Mathf.Max(0, _items[item] - count);
+            ItemRemoved?.Invoke(item, count);
         }
 
-        public void PushItem(Item item)
+        public bool CanRemoveItems(ItemInfo item, int count)
         {
-            if (CanAddItem(item) == false)
-                return;
-
-            _items.Add(item);
-
-            ItemPushed?.Invoke(item);
-        }
-
-        public void PushItems(IEnumerable<Item> items)
-        {
-            foreach (var item in items)
-                PushItem(item);
-        }
-
-        public Item PopItem(string id, bool isReverseSearch = true)
-        {
-            if (Enable == false)
-                return null;
-
-            if (ContainsItem(id) == false)
-            {
-                Debug.LogError($"Item with ID {id} is not in inventory");
-                return null;
-            }
-
-            var item = isReverseSearch
-                ? _items.FirstFromEnd(i => i.Info.ID == id)
-                : _items.First(i => i.Info.ID == id);
-
-            _items.Remove(item);
-
-            ItemPopped?.Invoke(item);
-
-            return item;
-        }
-
-        public Item[] PopItems(IEnumerable<ItemsStack> items, bool isReverseSearch = true)
-        {
-            if (Enable == false)
-                return Array.Empty<Item>();
-
-            var stacks = items as ItemsStack[] ?? items.ToArray();
-
-            if (ContainsItems(stacks) == false)
-            {
-                string errorText = stacks.Aggregate("The order cannot be completed:",
-                    (current, itemsStack) => current + $"\n{itemsStack.ItemInfo.Name} {itemsStack.Amount}");
-
-                Debug.LogError(errorText);
-                return null;
-            }
-
-            int arrayLength = stacks.Sum(i => i.Amount);
-
-            var result = new Item[arrayLength];
-            int counter = 0;
-
-            foreach (var itemsStack in stacks)
-            {
-                for (int i = 0; i < itemsStack.Amount; i++)
-                {
-                    result[counter] = PopItem(itemsStack.ItemInfo.ID, isReverseSearch);
-                    counter++;
-                }
-            }
-
-            return result;
-        }
-
-        public virtual bool CanAddItem(Item item)
-        {
-            if (Enable == false)
+            if (_items.ContainsKey(item) == false)
                 return false;
 
-            if (AmountItems + 1 > Info.Capacity)
-                return false;
-
-            if (Info.WhiteList.IsNullOrEmpty() == false)
-                return Info.WhiteList.Contains(item.Info);
-
-            if (Info.BlackList.IsNullOrEmpty() == false)
-                return Info.BlackList.Contains(item.Info) == false;
-
-            return true;
+            return _items[item] >= count;
         }
 
-        public bool CanAddItems(IEnumerable<Item> items)
+        public int GetCount(ItemInfo item)
         {
-            if (Enable == false)
-                return false;
-
-            var itemsArray = items as Item[] ?? items.ToArray();
-
-            if (AmountItems + itemsArray.Length > Info.Capacity)
-                return false;
-
-            foreach (var item in itemsArray)
-            {
-                if (CanAddItem(item) == false)
-                    return false;
-            }
-
-            return true;
-        }
-
-        public bool ContainsItem(string id)
-        {
-            return _items.Contains(i => i.Info.ID == id);
-        }
-
-        public bool ContainsItems(IEnumerable<ItemsStack> items)
-        {
-            foreach (var itemsStack in items)
-            {
-                if (_items.Count(i => i.Info == itemsStack.ItemInfo) < itemsStack.Amount)
-                    return false;
-            }
-
-            return true;
-        }
-
-        public int Counts(string id)
-        {
-            return _items.Count(i => i.Info.ID == id);
+            if (_items.ContainsKey(item) == false)
+                return 0;
+            
+            return _items[item];
         }
     }
 }
