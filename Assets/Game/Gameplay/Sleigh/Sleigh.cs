e7abbb6f977998d296639a0f446ab91397624bdd
Leonid Medvedev <leonid_med@mail.ru>
2024-06-29 19:05:52 +0300

    Update radial

--- Assets/Game/Gameplay/Sleigh/Sleigh.cs
+++ Assets/Game/Gameplay/Sleigh/Sleigh.cs
@@ -1,34 +1,8 @@
-using System.Collections.Generic;
-using DoubleDTeam.Containers;
-using DoubleDTeam.UI.Base;
-using Game.Gameplay.Interaction;
-using Game.UI;
 using UnityEngine;
 
 namespace Game.Gameplay
 {
-    public class Sleigh : InteractiveObject
+    public class Sleigh : MonoBehaviour
     {
-        [SerializeField] private string _name;
-        [SerializeField] private List<RadialButtonInfo> _operations;
-
-        private IUIManager _uiManager;
-        private RadialMenuArgument _radialMenuArgument;
-
-        protected void Awake()
-        {
-            _uiManager = Services.ProjectContext.GetModule<IUIManager>();
-
-            _radialMenuArgument = new RadialMenuArgument
-            {
-                Name = _name,
-                Buttons = _operations
-            };
-        }
-
-        public override void Interact()
-        {
-            _uiManager.OpenPage<RadialMenuPage, RadialMenuArgument>(_radialMenuArgument);
-        }
     }
 }
---
+++ Assets/Game/Gameplay/Sleigh/SleighInteractive.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using DoubleDTeam.Containers;
+using DoubleDTeam.UI.Base;
+using Game.Gameplay.Interaction;
+using Game.UI;
+using UnityEngine;
+
+namespace Game.Gameplay
+{
+    public class SleighInteractive : InteractiveObject
+    {
+        [SerializeField] private string _name;
+        [SerializeField] private List<RadialButtonInfo> _operations;
+
+        private IUIManager _uiManager;
+        private RadialMenuArgument _radialMenuArgument;
+
+        protected void Awake()
+        {
+            _uiManager = Services.ProjectContext.GetModule<IUIManager>();
+
+            _radialMenuArgument = new RadialMenuArgument
+            {
+                Name = _name,
+                Buttons = _operations
+            };
+        }
+
+        public override void Interact()
+        {
+            _uiManager.OpenPage<RadialMenuPage, RadialMenuArgument>(_radialMenuArgument);
+        }
+    }
+}
