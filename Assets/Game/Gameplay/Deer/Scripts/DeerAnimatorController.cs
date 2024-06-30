bd0a4de7c3d829286f7b20fcf30eca8a8876a7b8
GolubVR <74712885+GolubVR@users.noreply.github.com>
2024-06-30 23:33:18 +0700

    Теперь Олень - олень

---
+++ Assets/Game/Gameplay/Deer/Scripts/DeerAnimatorController.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using DoubleDTeam.Attributes;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(Animator))]
+public class DeerAnimatorController : MonoBehaviour
+{
+    [SerializeField, ReadOnlyProperty]
+    private Animator _animator;
+
+    [SerializeField] private NavMeshAgent _agent;
+    
+    private static readonly int Dead = Animator.StringToHash("Death");
+    private static readonly int Speed = Animator.StringToHash("Speed");
+    private static readonly int Eat = Animator.StringToHash("Eat");
+
+    private void OnValidate()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        SetSpeed(_agent.velocity.magnitude / _agent.speed);
+    }
+
+    public void StartDead()
+    {
+        _animator.SetTrigger(Dead);
+    }
+    
+    public void StartEat()
+    {
+        _animator.SetTrigger(Eat);
+    }
+    
+    public void SetSpeed(float value)
+    {
+        _animator.SetFloat(Speed, value);
+    }
+    
+}
