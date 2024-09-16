e8c27924f941578b17217f469be958b3a2b66209
Lexv0lk <Lexvolk2004@yandex.ru>
2024-09-16 22:08:00 +0400

    добавил ренейм оленей

--- Assets/Game/UI/Pages/DeerInfoPage.cs
+++ Assets/Game/UI/Pages/DeerInfoPage.cs
@@ -1,11 +1,16 @@
 ﻿using System;
+using DoubleDCore.Configuration;
+using DoubleDCore.Configuration.Base;
+using DoubleDCore.GameResources.Base;
 using DoubleDCore.TranslationTools;
 using DoubleDCore.TranslationTools.Extensions;
 using DoubleDCore.UI;
 using DoubleDCore.UI.Base;
 using Game.Gameplay;
+using Game.Gameplay.Scripts.Configs;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 using Zenject;
 
 namespace Game.UI.Pages
@@ -13,15 +18,40 @@ namespace Game.UI.Pages
     public class DeerInfoPage : MonoPage, IPayloadPage<DeerInfoPageArgument>
     {
         [SerializeField] private TextMeshProUGUI _text;
+        [SerializeField] private Image _deerIcon;
+        [SerializeField] private Image _deerFrame;
+        [SerializeField] private TMP_InputField _name;
 
         private GameInput _inputController;
-
         private DeerInfoPageArgument _context;
+        private DeerImagesConfig _deerImagesConfig;
 
         [Inject]
-        private void Init(GameInput inputController)
+        private void Init(GameInput inputController, IResourcesContainer resourcesContainer)
         {
+            _deerImagesConfig = resourcesContainer.GetResource<ConfigsResource>().GetConfig<DeerImagesConfig>();
             _inputController = inputController;
+        }
+
+        private void OnEnable()
+        {
+            _name.onValueChanged.AddListener(OnNameChanged);
+        }
+
+        private void OnDisable()
+        {
+            _name.onValueChanged.RemoveListener(OnNameChanged);
+        }
+
+        private void OnNameChanged(string newName)
+        {
+            if (_context == null)
+                return;
+            
+            if (newName != string.Empty)
+                _context.Info.Name = newName;
+
+            _name.text = _context.Info.Name;
         }
 
         public override void Initialize()
@@ -45,9 +75,13 @@ namespace Game.UI.Pages
 
             SetCanvasState(true);
 
-            _text.text = $"{_nameTranslate.GetText()} - {context.Info.Name}\n" +
-                         $"{_genderTranslate.GetText()} - {context.Info.Gender.ToText()}\n" +
-                         $"{_ageTranslate.GetText()} - {context.Info.Age.ToText()} ({context.Info.AgeDays} {_dayTranslate.GetText()})\n" +
+            _deerIcon.sprite = _deerImagesConfig.GetDeerImage(context.Info.Age);
+            _deerFrame.color = context.Info.Gender == GenderType.Female
+                ? _deerImagesConfig.FemaleFrameColor
+                : _deerImagesConfig.MaleFrameColor;
+
+            _name.text = $"{context.Info.Name}";
+            _text.text = $"{_ageTranslate.GetText()} - {context.Info.Age.ToText()} ({context.Info.AgeDays} {_dayTranslate.GetText()})\n" +
                          $"{_satietyTranslate.GetText()} - {Mathf.RoundToInt(context.Info.HungerDegree * 100)}%\n" +
                          $"{_statusTranslate.GetText()} - {context.Info.Status.ToText()}";
         }
---
+++ Assets/Game/UI/PointerHoverDetector.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+namespace Game.UI
+{
+    public class PointerHoverDetector : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    {
+        public UnityEvent PointEntered;
+        public UnityEvent PointExited;
+        
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            PointEntered?.Invoke();
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            PointExited?.Invoke();
+        }
+    }
+}
