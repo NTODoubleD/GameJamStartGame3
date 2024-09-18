23a71d3e068844101a4cc8cd140170b7dc390ce7
Lexv0lk <Lexvolk2004@yandex.ru>
2024-09-19 01:20:32 +0400

    Поменял логику цвета в слайдерах статуса

---
+++ Assets/Game/UI/EditIconController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using DG.Tweening;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Game.UI
+{
+    public class EditIconController : MonoBehaviour
+    {
+        [SerializeField] private Button _editButton;
+        [SerializeField] private TMP_InputField _inputField;
+
+        private void OnEnable()
+        {
+            _editButton.onClick.AddListener(OnButtonClicked);
+        }
+
+        private void OnDisable()
+        {
+            _editButton.onClick.RemoveListener(OnButtonClicked);
+        }
+
+        private void OnButtonClicked()
+        {
+            _inputField.Select();
+        }
+    }
+}
