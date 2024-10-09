7839b2a8e8fa0dfdbf28e5d33bdf539020101d32
Lexv0lk <Lexvolk2004@yandex.ru>
2024-10-09 18:09:24 +0400

    Added confirm page on deer killing

--- Assets/Game/Gameplay/Deer/Scripts/Stats/DeerKillController.cs
+++ Assets/Game/Gameplay/Deer/Scripts/Stats/DeerKillController.cs
@@ -1,11 +1,24 @@
 using DoubleDCore.Service;
+using DoubleDCore.UI.Base;
+using Game.Gameplay.Scripts.Configs;
+using Game.UI.Pages;
 using UnityEngine;
+using Zenject;
 
 namespace Game.Gameplay.Deers
 {
     public class DeerKillController : MonoService
     {
         [SerializeField] private CharacterAnimatorController _characterAnimatorController;
+        [SerializeField] private DeerKillConfig _deerKillConfig;
+
+        private IUIManager _uiManager;
+        
+        [Inject]
+        private void Init(IUIManager uiManager)
+        {
+            _uiManager = uiManager;
+        }
 
         public bool CanKill(Deer deer)
         {
@@ -16,12 +29,24 @@ namespace Game.Gameplay.Deers
         {
             if (CanKill(deer))
             {
-                _characterAnimatorController.AnimateKilling(() => ApplyKill(deer));
+                _uiManager.OpenPage<ConfirmPage, ConfirmPageArgument>
+                (new ConfirmPageArgument(_deerKillConfig.ConfirmTitle, _deerKillConfig.ConfirmMessage,
+                    () => StartKilling(deer), () => CancelKilling(deer)));
             }
             else
             {
                 Debug.LogError("CAN'T KILL THIS DEER");
             }
+        }
+
+        private void StartKilling(Deer deer)
+        {
+            _characterAnimatorController.AnimateKilling(() => ApplyKill(deer));
+        }
+
+        private void CancelKilling(Deer deer)
+        {
+            deer.EnterWalkingState();
         }
 
         private void ApplyKill(Deer deer)
---
+++ Assets/Game/UI/Pages/ConfirmPage.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using DoubleDCore.UI;
+using DoubleDCore.UI.Base;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Game.UI.Pages
+{
+    public class ConfirmPage : MonoPage, IPayloadPage<ConfirmPageArgument>
+    {
+        [SerializeField] private Button _confirmButton;
+        [SerializeField] private Button _cancelButton;
+        [SerializeField] private TMP_Text _title;
+        [SerializeField] private TMP_Text _message;
+
+        private ConfirmPageArgument _argument;
+        
+        public void Open(ConfirmPageArgument context)
+        {
+            _argument = context;
+            
+            _title.text = context.Title;
+            _message.text = context.Message;
+            
+            _confirmButton.onClick.AddListener(OnConfirmClicked);
+            _cancelButton.onClick.AddListener(OnCancelClicked);
+            
+            SetCanvasState(true);
+        }
+
+        private void OnConfirmClicked()
+        {
+            Close();
+            _argument.SuccessAction?.Invoke();
+        }
+
+        private void OnCancelClicked()
+        {
+            Close();
+            _argument.CancelAction?.Invoke();
+        }
+
+        public override void Close()
+        {
+            _confirmButton.onClick.RemoveListener(OnConfirmClicked);
+            _cancelButton.onClick.RemoveListener(OnCancelClicked);
+            SetCanvasState(false);
+        }
+    }
+    
+    public class ConfirmPageArgument
+    {
+        public string Title { get; }
+        public string Message { get; }
+        public Action SuccessAction { get; }
+        public Action CancelAction { get; }
+        
+        public ConfirmPageArgument(string title, string message, Action successAction, Action cancelAction)
+        {
+            Title = title;
+            Message = message;
+            SuccessAction = successAction;
+            CancelAction = cancelAction;
+        }
+    }
+}
