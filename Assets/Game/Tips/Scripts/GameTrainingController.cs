7f1bfb6501edfae61606f7e51fead2289d46d468
Leonid Medvedev <leonid_med@mail.ru>
2024-10-12 15:35:50 +0300

    Add training triggers

---
+++ Assets/Game/Tips/Scripts/GameTrainingController.cs
@@ -0,0 +1,42 @@
+﻿using DoubleDCore.UI.Base;
+using Game.UI.Data;
+using Game.UI.Pages;
+using Zenject;
+
+namespace Game.Tips
+{
+    public class GameTrainingController
+    {
+        private readonly IUIManager _uiManager;
+        private readonly GameInput _gameInput;
+
+        [Inject]
+        public GameTrainingController(IUIManager uiManager, GameInput gameInput)
+        {
+            _uiManager = uiManager;
+            _gameInput = gameInput;
+        }
+
+        public void StartTraining(TrainingInfo context)
+        {
+            _gameInput.Player.Disable();
+            _gameInput.Map.Disable();
+
+            _gameInput.UI.Enable();
+
+            _uiManager.OpenPage<TrainingPage, TrainingPageArgument>(new TrainingPageArgument
+            {
+                TrainingInfo = context,
+                OnClose = OnStopTraining
+            });
+        }
+
+        private void OnStopTraining()
+        {
+            _gameInput.Player.Enable();
+            _gameInput.Map.Enable();
+
+            _gameInput.UI.Disable();
+        }
+    }
+}
---
+++ Assets/Game/Tips/Scripts/Triggers/UnityEventTrainingTrigger.cs
@@ -0,0 +1,10 @@
+﻿namespace Game.Tips.Triggers
+{
+    public class UnityEventTrainingTrigger : TrainingTrigger
+    {
+        public void CallTraining()
+        {
+            StartTraining();
+        }
+    }
+}
