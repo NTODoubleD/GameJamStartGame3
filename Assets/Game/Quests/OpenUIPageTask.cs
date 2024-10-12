4ff546637fc0ca6165a74f6dfb0b12c79fa0a2b2
Leonid Medvedev <leonid_med@mail.ru>
2024-10-12 19:17:26 +0300

    Add OpenUIPageTask

---
+++ Assets/Game/Quests/OpenUIPageTask.cs
@@ -0,0 +1,33 @@
+﻿using DoubleDCore.UI.Base;
+using Game.Quests.Base;
+using Zenject;
+
+namespace Game.Quests
+{
+    public abstract class OpenUIPageTask<TPageType> : YakutSubTask where TPageType : IPage
+    {
+        private IUIManager _uiManager;
+
+        [Inject]
+        private void Init(IUIManager uiManager)
+        {
+            _uiManager = uiManager;
+        }
+
+        public override void Play()
+        {
+            _uiManager.PageOpened += UiManagerOnPageOpened;
+        }
+
+        public override void Close()
+        {
+            _uiManager.PageOpened -= UiManagerOnPageOpened;
+        }
+
+        private void UiManagerOnPageOpened(IPage page)
+        {
+            if (page is TPageType)
+                PerformQuest();
+        }
+    }
+}
