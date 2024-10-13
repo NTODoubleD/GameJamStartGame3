fdedaa3109e660339a4f75916c4576f6b3044a7e
Leonid Medvedev <leonid_med@mail.ru>
2024-10-13 06:32:56 +0300

    Add compass

---
+++ Assets/Game/Сompass/CompassController.cs
@@ -0,0 +1,92 @@
+using System.Collections.Generic;
+using DoubleDCore.QuestsSystem.Base;
+using Game.Quests;
+using Game.Quests.Base;
+using UnityEngine;
+using Zenject;
+
+namespace Game.Сompass
+{
+    public class CompassController : MonoBehaviour
+    {
+        [SerializeField] private CommpassView _view;
+
+        private bool _compassActive;
+
+        private bool CompassActive
+        {
+            get => _compassActive;
+            set
+            {
+                _compassActive = value;
+                _view.SetActive(value);
+            }
+        }
+
+        private IQuestController _questController;
+
+        [Inject]
+        private void Init(IQuestController questController)
+        {
+            _questController = questController;
+        }
+
+        public void OnEnable()
+        {
+            _questController.QuestIssued += OnQuestIssued;
+        }
+
+        public void OnDisable()
+        {
+            _questController.QuestIssued -= OnQuestIssued;
+        }
+
+        private void FixedUpdate()
+        {
+            if (HasTarget == false)
+            {
+                if (CompassActive)
+                    CompassActive = false;
+
+                return;
+            }
+
+            if (CompassActive == false)
+                CompassActive = true;
+
+            _view.UpdateCompass(_targets[0].transform.position);
+        }
+
+        private bool HasTarget => _targets.Count > 0;
+
+        private readonly List<VisitAreaTask> _targets = new();
+
+        private void OnQuestIssued(IQuest quest)
+        {
+            if (quest is not YakutQuest yakutQuest)
+                return;
+
+            CompassActive = false;
+
+            _targets.Clear();
+
+            foreach (var subTask in yakutQuest.SubTasks)
+            {
+                if (subTask is not VisitAreaTask visitAreaTask)
+                    continue;
+
+                visitAreaTask.QuestCompleted += OnTaskCompleted;
+                _targets.Add(visitAreaTask);
+            }
+        }
+
+        private void OnTaskCompleted(IQuest quest)
+        {
+            if (quest is not VisitAreaTask visitAreaTask)
+                return;
+
+            _targets.Find(y => y.Equals(visitAreaTask)).QuestCompleted -= OnTaskCompleted;
+            _targets.Remove(visitAreaTask);
+        }
+    }
+}
---
+++ Assets/Game/Сompass/UICompass.cs
@@ -0,0 +1,42 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+
+namespace Game.Сompass
+{
+    public class UICompass : CommpassView
+    {
+        private Image _compassImage;
+
+        private CharacterMover _characterMover;
+
+        [Inject]
+        private void Init(CharacterMover characterMover)
+        {
+            _characterMover = characterMover;
+        }
+
+        private void Awake()
+        {
+            _compassImage = GetComponent<Image>();
+        }
+
+        public override void UpdateCompass(Vector3 targetPosition)
+        {
+            var compassPosition = _characterMover.transform.position;
+
+            var direction = new Vector2(targetPosition.x, targetPosition.z) -
+                            new Vector2(compassPosition.x, compassPosition.z);
+
+            var angle = Vector2.SignedAngle(Vector2.right, direction);
+
+            _compassImage.transform.rotation = Quaternion.Euler(0, 0, angle);
+        }
+
+        public override void SetActive(bool active)
+        {
+            _compassImage.DOFade(active ? 1 : 0, 1f);
+        }
+    }
+}
